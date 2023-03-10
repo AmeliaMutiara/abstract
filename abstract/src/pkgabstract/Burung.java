89561df5dfef9dd28ab15d761298973e58164c30
AmeliaMutiara <98964756+AmeliaMutiara@users.noreply.github.com>
2023-03-10 13:10:14 +0700

    Add files via upload

---
+++ abstract/src/pkgabstract/Burung.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgabstract;
+
+/**
+ *
+ * @author zyrex
+ */
+public class Burung extends MahlukHidup{
+    void berjalan(){
+        System.out.println("burung berjalan");
+    }
+}
